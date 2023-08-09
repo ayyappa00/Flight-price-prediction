41d6a194bdf183a49e9917e4249c02ace7df2d72
ayyappa00 <141609655+ayyappa00@users.noreply.github.com>
2023-08-09 13:40:22 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,205 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+#Loading the dataset
+train_data = pd.read_excel("Data_Train.xlsx")
+train_data.dropna(inplace = True)
+train_data.isnull().sum()
+train_data["Journey_day"] = pd.to_datetime(train_data.Date_of_Journey, format="%d/%m/%Y").dt.day
+train_data["Journey_month"] = pd.to_datetime(train_data["Date_of_Journey"], format = "%d/%m/%Y").dt.month
+train_data.drop(["Date_of_Journey"], axis = 1, inplace = True)
+# Departure time is when a plane leaves the gate.
+# Similar to Date_of_Journey we can extract values from Dep_Time
+
+# Extracting Hours
+train_data["Dep_hour"] = pd.to_datetime(train_data["Dep_Time"]).dt.hour
+
+# Extracting Minutes
+train_data["Dep_min"] = pd.to_datetime(train_data["Dep_Time"]).dt.minute
+
+# Now we can drop Dep_Time as it is of no use
+train_data.drop(["Dep_Time"], axis = 1, inplace = True)
+train_data["Arrival_hour"] = pd.to_datetime(train_data.Arrival_Time).dt.hour
+
+# Extracting Minutes
+train_data["Arrival_min"] = pd.to_datetime(train_data.Arrival_Time).dt.minute
+
+# Now we can drop Arrival_Time as it is of no use
+train_data.drop(["Arrival_Time"], axis = 1, inplace = True)
+duration = list(train_data["Duration"])
+
+for i in range(len(duration)):
+    if len(duration[i].split()) != 2:    # Check if duration contains only hour or mins
+        if "h" in duration[i]:
+            duration[i] = duration[i].strip() + " 0m"   # Adds 0 minute
+        else:
+            duration[i] = "0h " + duration[i]           # Adds 0 hour
+
+duration_hours = []
+duration_mins = []
+for i in range(len(duration)):
+    duration_hours.append(int(duration[i].split(sep = "h")[0]))    # Extract hours from duration
+    duration_mins.append(int(duration[i].split(sep = "m")[0].split()[-1]))   # Extracts only minutes from duration
+# Adding duration_hours and duration_mins list to train_data dataframe
+
+train_data["Duration_hours"] = duration_hours
+train_data["Duration_mins"] = duration_mins
+train_data.drop(["Duration"], axis = 1, inplace = True)
+Airline = train_data[["Airline"]]
+
+Airline = pd.get_dummies(Airline, drop_first= True)
+
+Airline.head()
+Source = train_data[["Source"]]
+
+Source = pd.get_dummies(Source, drop_first= True)
+
+Source.head()
+Destination = train_data[["Destination"]]
+
+Destination = pd.get_dummies(Destination, drop_first = True)
+
+Destination.head()
+train_data["Route"]
+train_data.drop(["Route", "Additional_Info"], axis = 1, inplace = True)
+train_data["Total_Stops"].value_counts()
+train_data.replace({"non-stop": 0, "1 stop": 1, "2 stops": 2, "3 stops": 3, "4 stops": 4}, inplace = True)
+data_train = pd.concat([train_data, Airline, Source, Destination], axis = 1)
+data_train.drop(["Airline", "Source", "Destination"], axis = 1, inplace = True)
+test_data = pd.read_excel("Test_set.xlsx")
+print("Test data Info")
+print("-"*75)
+print(test_data.info())
+
+print()
+print()
+
+print("Null values :")
+print("-"*75)
+test_data.dropna(inplace = True)
+print(test_data.isnull().sum())
+test_data["Journey_day"] = pd.to_datetime(test_data.Date_of_Journey, format="%d/%m/%Y").dt.day
+test_data["Journey_month"] = pd.to_datetime(test_data["Date_of_Journey"], format = "%d/%m/%Y").dt.month
+test_data.drop(["Date_of_Journey"], axis = 1, inplace = True)
+
+# Dep_Time
+test_data["Dep_hour"] = pd.to_datetime(test_data["Dep_Time"]).dt.hour
+test_data["Dep_min"] = pd.to_datetime(test_data["Dep_Time"]).dt.minute
+test_data.drop(["Dep_Time"], axis = 1, inplace = True)
+
+# Arrival_Time
+test_data["Arrival_hour"] = pd.to_datetime(test_data.Arrival_Time).dt.hour
+test_data["Arrival_min"] = pd.to_datetime(test_data.Arrival_Time).dt.minute
+test_data.drop(["Arrival_Time"], axis = 1, inplace = True)
+duration = list(test_data["Duration"])
+
+for i in range(len(duration)):
+    if len(duration[i].split()) != 2:    # Check if duration contains only hour or mins
+        if "h" in duration[i]:
+            duration[i] = duration[i].strip() + " 0m"   # Adds 0 minute
+        else:
+            duration[i] = "0h " + duration[i]           # Adds 0 hour
+
+duration_hours = []
+duration_mins = []
+for i in range(len(duration)):
+    duration_hours.append(int(duration[i].split(sep = "h")[0]))    # Extract hours from duration
+    duration_mins.append(int(duration[i].split(sep = "m")[0].split()[-1]))
+test_data.drop(["Duration"], axis=1, inplace=True)
+print("Airline")
+print("-"*75)
+print(test_data["Airline"].value_counts())
+Airline = pd.get_dummies(test_data["Airline"], drop_first= True)
+
+print()
+
+print("Source")
+print("-"*75)
+print(test_data["Source"].value_counts())
+Source = pd.get_dummies(test_data["Source"], drop_first= True)
+
+print()
+
+print("Destination")
+print("-"*75)
+print(test_data["Destination"].value_counts())
+Destination = pd.get_dummies(test_data["Destination"], drop_first = True)
+
+# Additional_Info contains almost 80% no_info
+# Route and Total_Stops are related to each other
+test_data.drop(["Route", "Additional_Info"], axis = 1, inplace = True)
+
+# Replacing Total_Stops
+test_data.replace({"non-stop": 0, "1 stop": 1, "2 stops": 2, "3 stops": 3, "4 stops": 4}, inplace = True)
+data_test = pd.concat([test_data, Airline, Source, Destination], axis = 1)
+
+data_test.drop(["Airline", "Source", "Destination"], axis = 1, inplace = True)
+
+print()
+print()
+
+print("Shape of test data : ", data_test.shape)
+data_train.shape
+data_train.columns
+X = data_train.loc[:, ['Total_Stops', 'Journey_day', 'Journey_month', 'Dep_hour',
+       'Dep_min', 'Arrival_hour', 'Arrival_min', 'Duration_hours',
+       'Duration_mins', 'Airline_Air India', 'Airline_GoAir', 'Airline_IndiGo',
+       'Airline_Jet Airways', 'Airline_Jet Airways Business',
+       'Airline_Multiple carriers',
+       'Airline_Multiple carriers Premium economy', 'Airline_SpiceJet',
+       'Airline_Trujet', 'Airline_Vistara', 'Airline_Vistara Premium economy',
+       'Source_Chennai', 'Source_Delhi', 'Source_Kolkata', 'Source_Mumbai',
+       'Destination_Cochin', 'Destination_Delhi', 'Destination_Hyderabad',
+       'Destination_Kolkata', 'Destination_New Delhi']]
+X.head()
+y = data_train.iloc[:, 1]
+y.head()
+from sklearn.ensemble import ExtraTreesRegressor
+selection = ExtraTreesRegressor()
+selection.fit(X, y)
+print(selection.feature_importances_)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+from sklearn.ensemble import RandomForestRegressor
+reg_rf = RandomForestRegressor()
+reg_rf.fit(X_train, y_train)
+y_pred = reg_rf.predict(X_test)
+reg_rf.score(X_train, y_train)
+reg_rf.score(X_test, y_test)
+from sklearn import metrics
+print('MAE:', metrics.mean_absolute_error(y_test, y_pred))
+print('MSE:', metrics.mean_squared_error(y_test, y_pred))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+# RMSE/(max(DV)-min(DV))
+
+2090.5509/(max(y)-min(y))
+metrics.r2_score(y_test, y_pred)
+from sklearn.model_selection import RandomizedSearchCV
+#Randomized Search CV
+
+# Number of trees in random forest
+n_estimators = [int(x) for x in np.linspace(start = 100, stop = 1200, num = 12)]
+# Number of features to consider at every split
+max_features = ['auto', 'sqrt']
+# Maximum number of levels in tree
+max_depth = [int(x) for x in np.linspace(5, 30, num = 6)]
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10, 15, 100]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 5, 10]
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf}
+rf_random = RandomizedSearchCV(estimator = reg_rf, param_distributions = random_grid,scoring='neg_mean_squared_error', n_iter = 10, cv = 5, verbose=2, random_state=42, n_jobs = 1)
+rf_random.fit(X_train,y_train)
+rf_random.best_params_
+prediction = rf_random.predict(X_test)
+import pickle
+# open a file, where you ant to store the data
+file = open('flight_rf.pkl', 'wb')
+
+# dump information to that file
+pickle.dump(reg_rf, file)
